a28eb681ad65d7fe225f318156bcd3aabf90688d
Mustafa Turan <mustafaturan.net@gmail.com>
2018-02-17 18:43:24 -0800

    Bump version to 0.1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EventBus.Logger.Mixfile do
   def project do
     [
       app: :event_bus_logger,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env),
       start_permanent: Mix.env == :prod,
