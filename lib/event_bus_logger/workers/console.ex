3921de7ccbd2a4e3bd5ef5755e89b347391e49d8
Mustafa Turan <mustafaturan.net@gmail.com>
2018-06-07 19:39:07 -0700

    Refactor logger

--- lib/event_bus_logger/workers/console.ex
+++ lib/event_bus_logger/workers/console.ex
@@ -30,17 +30,16 @@ defmodule EventBus.Logger.Worker.Console do
   @doc false
   def handle_cast({topic, id}, state) do
     event = EventBus.fetch_event({topic, id})
-    log(event)
+    log(event, light_logging: Config.light_logging?())
     EventBus.mark_as_completed({EventBus.Logger, topic, id})
     {:noreply, state}
   end
 
-  defp log(event) do
-    case Config.light_logging?() do
-      true  ->
-        Logger.log(Config.level(), fn -> "[EVENTBUS] #{event.topic}" end)
-      false ->
-        Logger.log(Config.level(), fn -> inspect(event) end)
-    end
+  defp log(event, light_logging: true) do
+    Logger.log(Config.level(), fn -> "[EVENTBUS]\s#{event.topic}" end)
+  end
+
+  defp log(event, _) do
+    Logger.log(Config.level(), fn -> "[EVENTBUS]\s" <> inspect(event) end)
   end
 end
