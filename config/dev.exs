f9a35241aa33a378b8d2160010f8aa0e1ff1778b
Mustafa Turan <mustafaturan.net@gmail.com>
2018-06-07 19:38:27 -0700

    Update config

--- config/dev.exs
+++ config/dev.exs
@@ -1,12 +1,1 @@
 use Mix.Config
-
-config :event_bus,
-  observables: [
-    :register_topic,
-    :unregister_topic,
-    :subscribe,
-    :unsubscribe,
-    :mark_as_completed,
-    :mark_as_skipped
-  ],
-  id_generator: fn -> :base64.encode(:crypto.strong_rand_bytes(8)) end
